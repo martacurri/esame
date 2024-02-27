6bb5ba265d0cdbaaa8db309a76790da65f773657
martacurri <148890867+martacurri@users.noreply.github.com>
2024-02-27 17:50:59 +0100

    Add files via upload

---
+++ bash_esame.sh
@@ -0,0 +1,17 @@
+'''Bash script'''
+
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/main/Nemo_6670.dat
+if [ $? -eq 0 ]
+    then 
+        echo OK
+    else 
+        echo FAULT
+fi
+ls -l Nemo_6670.dat
+
+export DATA_FILE_PATH='pwd'/Nemo_6670.dat
+echo $DATA_FILE_PATH
+
+python3 esame.py "$DATA_FILE_PATH"
