68332d653495588d217a97406fba98738c29c396
martacurri <148890867+martacurri@users.noreply.github.com>
2024-02-28 15:11:30 +0100

    Add files via upload

--- ait_script2.sh
+++ ait_script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Definisci il percorso completo della cartella di destinazione
+#Definisco il percorso completo della cartella di destinazione
 destination_folder="$HOME/Desktop/Directory_esame"
 
-# Scarico il file Nemo_6670.dat da GitHub
+#Scarico il file Nemo_6670.dat da GitHub
 wget https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/main/Nemo_6670.dat
 
-# Verifica se il download è stato completato con successo
+#Verifico se il download è stato completato con successo
 if [ $? -eq 0 ] 
     then
         echo "Download completato con successo."
@@ -15,14 +15,14 @@ if [ $? -eq 0 ]
         exit 1
 fi
 
-# Ottieni il percorso completo del file scaricato
+#Percorso completo del file scaricato
 DATA_FILE_PATH=$(pwd)/Nemo_6670.dat
 echo "Percorso del file scaricato: $DATA_FILE_PATH"
 
-# Eseguo lo script Python ait_esame.py passando il percorso del file come argomento
+#Eseguo lo script Python ait_esame.py passando il percorso del file come argomento
 python3 ait_esame.py "$DATA_FILE_PATH"
 
-# Verifico lo stato di uscita dello script Python
+#Verifico lo stato di uscita dello script Python
 if [ $? -eq 0 ]; then
     echo "Lo script Python ha completato l'esecuzione con successo."
 else
