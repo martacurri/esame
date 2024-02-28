6a77d4887581d6627e77ca1d0f01e1eff3cb03de
martacurri <148890867+martacurri@users.noreply.github.com>
2024-02-28 13:56:40 +0100

    Add files via upload

---
+++ ait_script1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Directory di destinazione per l'applicazione
+directory="$HOME/Desktop/Directory_esame"
+
+# Crea la directory di destinazione se non esiste
+mkdir -p "$directory"
+
+# URL del repository GitHub
+github_repository="https://github.com/MartaCurri/esame"
+
+# Copia dei file dell'applicazione dalla repository GitHub nella directory di destinazione
+echo "Clonazione del repository GitHub."
+git clone "$github_repository" "$directory"
+
+# Modifica del PythonPath e del PATH di sistema
+echo "Modifica del PythonPath e del PATH di sistema."
+echo "export PYTHONPATH=\$PYTHONPATH:$directory" >> ~/.bashrc
+echo "export PATH=\$PATH:$directory" >> ~/.bashrc
+source ~/.bashrc
+
+# Impostazione dei permessi di esecuzione per lo script di avvio
+echo "Impostazione dei permessi di esecuzione per lo script di avvio."
+chmod +x "$directory/ait_script2.sh" 
+
+echo "Installazione completata con successo."
+
+# Entra nella directory giusta
+cd "$directory"
+
